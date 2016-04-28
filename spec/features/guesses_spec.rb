63f2f61e458c00eac8be09e1d87b479455dfaeb8
Raghu Betina <rbetina@gmail.com>
2016-04-28 11:15:39 -0500

    Add some tests

---
+++ spec/features/guesses_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+feature "The index page" do
+  scenario "has a functional RCAV", points: 5 do
+    visit "/all_guesses"
+
+    expect(page).to have_selector("h1", text: "A Quick Puzzle to Test Your Problem Solving")
+  end
+
+  scenario "has two forms", points: 5 do
+    visit "/all_guesses"
+
+    expect(page).to have_selector("form", count: 2)
+  end
+
+  scenario "has the first sequence", points: 5 do
+    visit "/all_guesses"
+
+    expect(page).to have_selector("li", text: "2, 4, 8 Obeys the rule")
+  end
+
+
+  scenario "has a reset link", points: 5 do
+    visit "/all_guesses"
+
+    expect(page).to have_link("Reset")
+  end
+
+end
